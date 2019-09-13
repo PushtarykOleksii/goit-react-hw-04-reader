8254f022caebfb06872b20c54de0786919b819a8
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-09-13 17:23:44 +0300

    Updates

---
+++ precache-manifest.afa9502d19259846940e8c0a60e588d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a0cbf21977ef7b1ca322cd669c4dea",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "1df9060bacdf611eb52d",
+    "url": "/goit-react-hw-04-reader/static/css/main.6e003eb3.chunk.css"
+  },
+  {
+    "revision": "268419a4fe087545612b",
+    "url": "/goit-react-hw-04-reader/static/js/2.fcb4a2b7.chunk.js"
+  },
+  {
+    "revision": "1df9060bacdf611eb52d",
+    "url": "/goit-react-hw-04-reader/static/js/main.1847a125.chunk.js"
+  },
+  {
+    "revision": "fae253dca44c2159d04f",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.877735af.js"
+  }
+]);
